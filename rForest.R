78983d6f522d9b7fe38d517205ee3cbf67ea4849
ajawid158 <ajawid@auaf.edu.af>
2024-04-08 02:06:03 +0430

    clustering8424

--- rForest.R
+++ rForest.R
@@ -30,22 +30,24 @@ spredict=predict(rfm, rftest)
 
 #Confusion matrix
 cm=table(rftest$Species, spredict)
+cm
 cac=sum(diag(cm))/sum(cm)
 cac
 
 
 ####+++++++++++++
-#2. random forest Regression
+#2. Random forest Regression
 tip=read.csv("tips.csv")
 head(tip)
 #RF Model 
 rfrm=randomForest(x=tip[2],
-                  y=tip$tip)
+                  y=tip$tip, 
+                  ntree = 500)
 plot(rfrm)
 min(rfrm$mse)
 #Predict
 y_pre=predict(rfrm, newdata = data.frame(total_bill=30))
-
+y_pre
 #plot using ggplot2
 x_grid=seq(min(tip$total_bill), max(tip$total_bill), 0.01)
 
